0d583a96982fed93e30b4b037dbf672447ec1dcf
Sasha-Buzo <aliaksandra.buzo@gmail.com>
2026-05-11 22:51:20 +0300

    Знакомство с PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,11 +1,14 @@
 from playwright.sync_api import Page, expect
 
 from pages.base_page import BasePage
+from components.navigation.navbar_component import NavbarComponent
 
 
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.navbar = NavbarComponent(page)
 
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
